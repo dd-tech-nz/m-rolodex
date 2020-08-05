261dc964110a9c7a7062ce09ba4008bcd9bcbc25
dd-tech-nz <ddumolo@hotmail.com>
2020-08-05 13:06:09 +1200

    Updates

---
+++ precache-manifest.297af8239db3e819293f477f992c2464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f188769ce9b0e76a68be362ad36085",
+    "url": "/m-rolodex/index.html"
+  },
+  {
+    "revision": "4eb2696f019353197c85",
+    "url": "/m-rolodex/static/css/main.0f5d12c3.chunk.css"
+  },
+  {
+    "revision": "27172ee4d3d9cbe4cb9c",
+    "url": "/m-rolodex/static/js/2.de913a05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/m-rolodex/static/js/2.de913a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb2696f019353197c85",
+    "url": "/m-rolodex/static/js/main.78f1b799.chunk.js"
+  },
+  {
+    "revision": "5dbd4ba07771fec2b52b",
+    "url": "/m-rolodex/static/js/runtime-main.cb359722.js"
+  }
+]);
